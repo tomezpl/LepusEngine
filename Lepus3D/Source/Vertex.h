3bf17cadbdf9fb65be4c754fe2f3bcbe3e2c24ed
Tomek Zając <tomezpl@gmail.com>
2017-04-12 02:02:44 +0100

    Added operators and containers for Vertex class
    VertexArray as a typedef for a vector of vertices, VertexPack to store
    vertex data and size.

--- Lepus3D/Source/Vertex.h
+++ Lepus3D/Source/Vertex.h
@@ -10,6 +10,22 @@ namespace LepusEngine
 		public:
 			float x, y, z, r, g, b;
 			Vertex(float X = 0.f, float Y = 0.f, float Z = 0.f, float R = 0.f, float G = 0.f, float B = 0.f) { x = X; y = Y; z = Z; r = R; g = G; b = B; };
+			Vertex operator=(Vertex& rs) { x = rs.x; y = rs.y; z = rs.z; r = rs.r; g = rs.g; b = rs.b; return *this;  };
+			bool operator==(Vertex& rs) { return x == rs.x && y == rs.y && z == rs.z && r == rs.r && g == rs.g && b == rs.b; };
+			bool operator!=(Vertex& rs) { return !(*this == rs); };
+		};
+
+		typedef std::vector<Vertex> VertexArray;
+
+		class VertexPack
+		{
+		private:
+			unsigned int m_Size;
+		public:
+			Vertex* data;
+			unsigned int size() { return m_Size; };
+			VertexPack() { data = new Vertex(); m_Size = 0; };
+			VertexPack(VertexArray vertexData) { data = new Vertex[m_Size = vertexData.size()]; for (auto i = 0; i < m_Size; i++) { data[i] = vertexData[i]; } };
 		};
 	}
 }
