0c975bbda52f7154f74120e48e05ae85ec48dfa4
Tomek Zając <tomezpl@gmail.com>
2017-04-12 02:06:22 +0100

    Updated Demo app to use Mesh

--- LepusDemo/Source/main.cpp
+++ LepusDemo/Source/main.cpp
@@ -7,30 +7,26 @@ int main()
 {
 	Lepus3D::RenderEngine engine("LepusDemo", 800, 600);
 
-	// Vertex declaration
-	/*Vertex verts[] = {
-		Vertex(-0.5f, 0.5f, 0.0f), // Top left
-		Vertex(0.5f, 0.5f, 0.0f), // Top right
-		Vertex(-0.5f, -0.5f, 0.0f), // Bottom left
-		Vertex(0.5f, -0.5f, 0.0f) }; // Bottom right*/
-
 	// Some diamond-like shape:
-	Vertex verts[] = {
-		Vertex(0.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f),
+	VertexArray verts = {
+		Vertex(0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f),
 		Vertex(-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f),
 		Vertex(0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f),
-		Vertex(0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f) };
-	GLuint indices[] = {
-		0, 1, 2,
-		1, 2, 3 };
+		Vertex(-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f),
+		Vertex(0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f),
+		Vertex(0.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f) };
 
 	Lepus3D::Material testMat("Test material", "PerVertexUnlit");
-	//testMat.SetAttributeF4("_Color", new float[4]{ 0.75f, 0.75f, 0.75f, 1.0f });
+	Lepus3D::Mesh testMesh(verts);
+	testMat.SetAttributeF4("_Color", new float[4]{ 0.75f, 0.75f, 0.75f, 1.0f });
+
+	sf::Clock timer;
 
 	bool running = true;
 	while (running)
 	{
-		engine.DrawVertices(verts, sizeof(verts), indices, sizeof(indices) / sizeof(GLuint), testMat);
+		testMat.SetAttributeF("_Time", timer.getElapsedTime().asSeconds());
+		engine.DrawMesh(testMesh, testMat);
 		running = engine.Update();
 	}
 	engine.Shutdown();
