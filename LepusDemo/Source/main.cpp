40828e79e65bc3d0de291edaa759eb2ad6ad824f
Tomek Zając <tomezpl@gmail.com>
2017-04-13 01:01:43 +0100

    Updated demo to use textures
    Obtain your own texture image files, these are not provided with the
    repo. Create a "Content" folder in root dir.

--- LepusDemo/Source/main.cpp
+++ LepusDemo/Source/main.cpp
@@ -7,18 +7,24 @@ int main()
 {
 	Lepus3D::RenderEngine engine("LepusDemo", 800, 600);
 
-	// Some diamond-like shape:
-	VertexArray verts = {
-		Vertex(0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f),
-		Vertex(-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f),
-		Vertex(0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f),
-		Vertex(-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f),
-		Vertex(0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f),
-		Vertex(0.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f) };
+	VertexArray verts = 
+	{
+				// Positions			// Colours		 // Texture 
+														 // coords
+		Vertex(0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	1.0f, 1.0f), // Top-right corner
+		Vertex(0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	1.0f, 0.0f), // Bottom-right corner
+		Vertex(-0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	0.0f, 1.0f), // Top-left corner
+		Vertex(-0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	0.0f, 0.0f)  // Bottom-left corner
+	};
 
 	Lepus3D::Material testMat("Test material", "PerVertexUnlit");
-	Lepus3D::Mesh testMesh(verts);
-	testMat.SetAttributeF4("_Color", new float[4]{ 0.75f, 0.75f, 0.75f, 1.0f });
+	Lepus3D::Mesh testMesh(verts, true);
+	std::vector<unsigned int> indices = { 2, 0, 3, 0, 1, 3 };
+	testMesh.SetIndices(indices);
+
+	// Be sure to get your own images, these are not provided with the Git repository
+	Lepus3D::Texture2D firstTx("container.jpg"); // Loads from Solution/Content/
+	testMat.SetAttributeTex("_Texture1", firstTx);
 
 	sf::Clock timer;
 
