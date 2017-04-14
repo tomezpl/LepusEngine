14ddd494228099651385ebf9acc3a63d769fe153
Tomek Zając <tomezpl@gmail.com>
2017-04-14 16:40:07 +0100

    Updated demo app to use Transform

--- LepusDemo/Source/main.cpp
+++ LepusDemo/Source/main.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	Lepus3D::RenderEngine engine("LepusDemo", 800, 600);
 
-	/*VertexArray verts = 
+	VertexArray verts = 
 	{
 				// Positions			// Colours		 // Texture 
 														 // coords
@@ -15,24 +15,30 @@ int main()
 		Vertex(0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	1.0f, 0.0f), // Bottom-right corner
 		Vertex(-0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	0.0f, 1.0f), // Top-left corner
 		Vertex(-0.5f, -0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	0.0f, 0.0f)  // Bottom-left corner
-	};*/
+	};
 
 	Lepus3D::Material testMat("Test material", "PerVertexUnlit");
+	//Lepus3D::Mesh testMesh(verts, true);
 	Lepus3D::PlaneMesh testMesh;
-	std::vector<unsigned int> indices = { 2, 0, 3, 0, 1, 3 };
+	//std::vector<unsigned int> indices = { 2, 0, 3, 0, 1, 3 };
 	//testMesh.SetIndices(indices);
 
 	// Be sure to get your own images, these are not provided with the Git repository
 	Lepus3D::Texture2D firstTx("container.jpg"); // Loads from Solution/Content/
 	testMat.SetAttributeTex("_Texture1", firstTx);
 
+	Lepus3D::Transform transform;
+
 	sf::Clock timer;
 
 	bool running = true;
 	while (running)
 	{
-		testMat.SetAttributeF("_Time", timer.getElapsedTime().asSeconds());
-		engine.DrawMesh(testMesh, testMat);
+		float timeSeconds = timer.getElapsedTime().asSeconds();
+		testMat.SetAttributeF("_Time", timeSeconds);
+		transform.SetPosition(Vector3(0.5f, -0.5f, 0.f));
+		transform.SetRotation(Vector3(0.f, 0.f, timeSeconds * 50.f));
+		engine.DrawMesh(testMesh, testMat, transform);
 		running = engine.Update();
 	}
 	engine.Shutdown();
