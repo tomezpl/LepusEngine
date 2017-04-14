62c5fd469e374477dcb4f8924d1f35a565359d76
Tomek Zając <tomezpl@gmail.com>
2017-04-14 16:36:18 +0100

    Added a Vector class

---
+++ Lepus3D/Source/Transform/Vector.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace LepusEngine
+{
+	namespace Lepus3D
+	{
+		class Vector2 {
+		public:
+			float x, y;
+			Vector2(float X = 0.f, float Y = 0.f) { x = X; y = Y; };
+			glm::vec2 vec2() { return glm::vec2(x, y); };
+			Vector2& operator=(Vector2& rs) { return Vector2(rs.x, rs.y); };
+			Vector2 Zero() { return Vector2(); };
+		};
+		
+		class Vector3 : public Vector2 {
+		public:
+			float z;
+			Vector3(float X = 0.f, float Y = 0.f, float Z = 0.f) : Vector2(X, Y) { z = Z; };
+			glm::vec3 vec3() { return glm::vec3(x, y, z); };
+			Vector3& operator=(Vector3& rs) { return Vector3(rs.x, rs.y, rs.z); };
+			Vector3 Zero() { return Vector3(); };
+		};
+
+		class Vector4 : public Vector3 {
+		public:
+			float w;
+			Vector4(float X = 0.f, float Y = 0.f, float Z = 0.f, float W = 0.f) : Vector3(X, Y, Z) { w = W; };
+			glm::vec4 vec4() { return glm::vec4(x, y, z, w); };
+			Vector4& operator=(Vector4& rs) { return Vector4(rs.x, rs.y, rs.z, rs.w); };
+			Vector4 Zero() { return Vector4(); };
+		};
+	}
+}
