168785cd6247d09ac361c873f7a281cbecbb85fa
Tomek Zając <tomezpl@gmail.com>
2017-04-28 21:47:50 +0100

    Cleaned Vector code and added some utility functions

--- Lepus3D/Source/Transform/Vector.h
+++ Lepus3D/Source/Transform/Vector.h
@@ -10,27 +10,36 @@ namespace LepusEngine
 		public:
 			float x, y;
 			Vector2(float X = 0.f, float Y = 0.f) { x = X; y = Y; };
+			Vector2(glm::vec2 v) { x = v.x; y = v.y; };
 			glm::vec2 vec2() { return glm::vec2(x, y); };
-			Vector2& operator=(Vector2& rs) { Vector2 ret(rs.x, rs.y); return ret; };
-			Vector2 Zero() { return Vector2(); };
+			Vector2& operator=(Vector2& rs) { return rs; };
+			static Vector2 Create(float X = 0.f, float Y = 0.f) { Vector2 ret(X, Y); return ret; }
+			void Set(float X = 0.f, float Y = 0.f) { x = X; y = Y; };
+			static Vector2 Zero() { return Vector2(); };
 		};
 		
 		class Vector3 : public Vector2 {
 		public:
 			float z;
 			Vector3(float X = 0.f, float Y = 0.f, float Z = 0.f) : Vector2(X, Y) { z = Z; };
+			Vector3(glm::vec3 v) { x = v.x; y = v.y; z = v.z; };
 			glm::vec3 vec3() { return glm::vec3(x, y, z); };
-			Vector3& operator=(Vector3& rs) { Vector3 ret(rs.x, rs.y, rs.z); return ret; };
-			Vector3 Zero() { return Vector3(); };
+			Vector3& operator=(Vector3& rs) { return rs; };
+			static Vector3 Create(float X = 0.f, float Y = 0.f, float Z = 0.f) { Vector3 ret(X, Y, Z); return ret; }
+			void Set(float X = 0.f, float Y = 0.f, float Z = 0.f) { x = X; y = Y; z = Z; };
+			static Vector3 Zero() { return Vector3(); };
 		};
 
 		class Vector4 : public Vector3 {
 		public:
 			float w;
 			Vector4(float X = 0.f, float Y = 0.f, float Z = 0.f, float W = 0.f) : Vector3(X, Y, Z) { w = W; };
+			Vector4(glm::vec4 v) { x = v.x; y = v.y; z = v.z; w = v.w; };
 			glm::vec4 vec4() { return glm::vec4(x, y, z, w); };
-			Vector4& operator=(Vector4& rs) { Vector4 ret(rs.x, rs.y, rs.z, rs.w); return ret; };
-			Vector4 Zero() { return Vector4(); };
+			Vector4& operator=(Vector4& rs) { return rs; };
+			static Vector4 Create(float X = 0.f, float Y = 0.f, float Z = 0.f, float W = 0.f) { Vector4 ret(X, Y, Z, W); return ret; }
+			void Set(float X = 0.f, float Y = 0.f, float Z = 0.f, float W = 0.f) { x = X; y = Y; z = Z; w = W; };
+			static Vector4 Zero() { return Vector4(); };
 		};
 	}
 }
