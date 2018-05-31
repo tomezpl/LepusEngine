fffb228d7e0d5968a0e2b4af61b66d79ee99d046
Tomasz Zając <tomezpl@gmail.com>
2018-05-31 16:23:31 +0100

    Added Plane normals and made Renderable & Mesh friends

--- Lepus3D/Source/Mesh.h
+++ Lepus3D/Source/Mesh.h
@@ -10,6 +10,7 @@ namespace LepusEngine
 	{
 		class Mesh {
 			friend class RenderEngine;
+			friend class Renderable;
 		private:
 			VertexArray m_Vertices;
 			std::vector<unsigned int> m_Indices;
@@ -28,10 +29,10 @@ namespace LepusEngine
 		class PlaneMesh : public Mesh {
 		public:
 			PlaneMesh() : Mesh(VertexArray {
-				Vertex(-0.5f, 0.5f, 0.f, 1.f, 1.f, 1.f, 0.0f, 1.0f), // Top-left
-				Vertex(0.5f, 0.5f, 0.f, 1.f, 1.f, 1.f, 1.0f, 1.0f), // Top-right
-				Vertex(-0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f, 0.0f, 0.0f), // Bottom-left
-				Vertex(0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f, 1.0f, 0.0f) // Bottom-right
+				Vertex(-0.5f, 0.5f, 0.f, 1.f, 1.f, 1.f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f), // Top-left
+				Vertex(0.5f, 0.5f, 0.f, 1.f, 1.f, 1.f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f), // Top-right
+				Vertex(-0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f), // Bottom-left
+				Vertex(0.5f, -0.5f, 0.f, 1.f, 1.f, 1.f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f) // Bottom-right
 			}) {
 				this->SetIndices(std::vector<unsigned int>{0, 1, 2, 1, 3, 2});
 			};
