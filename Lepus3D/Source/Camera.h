128b79d7c8f2d81966d7ca1643e41bdf51dfa1a6
Tomasz Zając <tomezpl@gmail.com>
2019-03-21 13:37:38 +0000

    Cleans up Camera code

--- Lepus3D/Source/Camera.h
+++ Lepus3D/Source/Camera.h
@@ -15,28 +15,55 @@ namespace LepusEngine
 {
 	namespace Lepus3D
 	{
+		// Base camera class. Completely static, only moved programmatically.
 		class Camera
 		{
 		protected:
+			// Transformation values
 			Transform m_Transform;
+
+			// Target and relative vector values
 			Vector3 m_Target, m_Right, m_Up;
+
+			// Calculates relative vectors based on target
 			void _CalcVectors();
+
+			// Initialises instance
 			void _Init();
 		public:
+			// Default constructor. Initialises the camera looking at (0.0, 0.0, 0.0).
 			Camera();
+
+			// Initialises the camera with a transform (position & rotation), and calculates relative vectors to reflect it.
 			Camera(Transform transform);
+
+			// Assigns new transform to use for calculations.
+			// transform is passed by copy, so you have to call this on each transform update.
 			void SetTransform(Transform transform);
+
+			// Returns the view matrix for this camera. This can be passed to RenderEngine for drawing calculations.
 			glm::mat4 GetView();
+
+			// Returns the target vector (where the camera is pointing)
 			Vector3 GetViewVector();
+
+			// Returns current transform
 			Transform GetTransform();
 		};
 
+		// Base class for input-controlled camera. Requires handle to app window.
 		class InputCamera : public Camera {
 		protected:
-			GLFWwindow* m_Wnd;
+			GLFWwindow* m_Wnd; // Pointer to window handle
 		public:
+			// Default constructor
 			InputCamera() : Camera() {};
+
+			// Initialises camera with a transform.
+			// See Camera::Camera(Transform)
 			InputCamera(Transform transform) : Camera(transform) {};
+			
+			// Sets window handle
 			virtual void SetWindow(GLFWwindow* window);
 		};
 	}
--- Lepus3D/Source/Camera/Camera.cpp
+++ Lepus3D/Source/Camera/Camera.cpp
@@ -3,6 +3,7 @@
 using namespace LepusEngine;
 using namespace Lepus3D;
 
+// Default constructor. Initialises the instance and calculates initial vectors.
 Camera::Camera()
 {
 	this->_Init();
@@ -10,46 +11,67 @@ Camera::Camera()
 
 void Camera::_Init()
 {
-	Vector3 tmp = Vector3::Zero();
-	m_Target = tmp;
+	Vector3 defaultTarget = Vector3::Zero();
+	m_Target = defaultTarget;
+
+	// Calculate right & up vectors for this target
 	this->_CalcVectors();
 }
 
 void Camera::_CalcVectors()
 {
+	// Set upward vector
 	m_Up.Set(0.f, 1.f, 0.f);
-	// this might break because Up is not always gonna be normalised
-	auto camPos = m_Transform.GetPosition().vec3();
-	Vector3 tmp = Vector3(glm::normalize(glm::cross(m_Up.vec3(), glm::normalize(camPos - m_Target.vec3()))));
-	m_Right = tmp;
+	
+	// Get current camera position from transform
+	glm::vec3 camPos = m_Transform.GetPosition().vec3();
+	
+	// Calculate new right-vector using upward vector, position and target
+	Vector3 newRight = Vector3(glm::normalize(glm::cross(m_Up.vec3(), glm::normalize(camPos - m_Target.vec3()))));
+	m_Right = newRight;
 }
 
 Camera::Camera(Transform transform) : Camera()
 {
+	// Assign transform
 	this->SetTransform(transform);
+
+	// Initialise instance
 	this->_Init();
 }
 
 void Camera::SetTransform(Transform transform)
 {
+	// Copy transform values
 	m_Transform = transform;
 }
 
 glm::mat4 Camera::GetView()
 {
+	// Calculate vectors
 	this->_CalcVectors();
-	auto camPos = m_Transform.GetPosition().vec3();
-	//auto target = m_Target.vec3();
-	auto rotation = m_Transform.GetRotation().vec3();
+
+	// Get current camera position and rotation vectors from transform
+	glm::vec3 camPos = m_Transform.GetPosition().vec3();
+	glm::vec3 rotation = m_Transform.GetRotation().vec3();
+
+	// Move target vector based on rotation
 	Vector3 target(
 		cos(glm::radians(rotation.x)) * cos(glm::radians(rotation.y)),
 		sin(glm::radians(rotation.x)),
 		cos(glm::radians(rotation.x)) * sin(glm::radians(rotation.y)));
-	auto normTarget = glm::normalize(target.vec3());
+
+	// Normalise target vector
+	glm::vec3 normTarget = glm::normalize(target.vec3());
+
+	// Update target vector with new values
 	m_Target.x = normTarget.x;
 	m_Target.y = normTarget.y;
 	m_Target.z = normTarget.z;
-	auto up = m_Up.vec3();
+
+	glm::vec3 up = m_Up.vec3();
+
+	// Return view/lookAt matrix
 	glm::mat4 ret = glm::lookAt(camPos, camPos + normTarget, up);
 	return ret;
 }
