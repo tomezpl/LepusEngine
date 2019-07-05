57b7e59e27464dbe91f300526a9b6975acd5ad93
Tomasz Zając <tomezpl@gmail.com>
2019-07-05 18:04:26 +0100

    Adds comments to Scene

--- Lepus3D/Source/Scene.h
+++ Lepus3D/Source/Scene.h
@@ -7,6 +7,9 @@
 
 namespace LepusEngine {
 	namespace Lepus3D {
+		// Scene class
+		// Stores references to objects that appear in or contribute to the rendered scene (e.g. Renderables, Lights)
+		// Describes Ambient Lighting properties.
 		class Scene {
 		friend class RenderEngine;
 		private:
@@ -15,27 +18,40 @@ namespace LepusEngine {
 			Color m_AmbientColor;
 			float m_AmbientIntensity;
 		public:
+			// Default constructor. Creates a white ambient light with 0.5 intensity.
 			Scene();
 
-			// Returns the index of the newly added mesh
+			// Adds mesh to the Scene and wraps it in a Renderable.
+			// Returns the index of the newly added mesh.
 			int AddMesh(Mesh mesh, Transform t = Transform(Vector3::Zero(), Vector3::Zero(), Vector3::Create(1.f, 1.f, 1.f)));
+
+			// Adds mesh to the Scene, provided it's already wrapped in a Renderable object.
+			// Returns the index of the newly added mesh.
 			int AddMesh(Renderable* renderable);
 
-			// Returns the index of the newly added light
+			// Adds light to the Scene.
+			// Returns the index of the newly added light.
 			int AddLight(Light* light);
 
 			// Returns the length of the scene's ObjArr
 			int GetRenderableCount();
 
+			// Returns the length of the scene's LightArr
 			int GetLightCount();
 
 			// Removes a mesh from the scene at a specified index
 			void RemoveMesh(int index);
 
+			// Removes a light from the scene at a specified index
 			void RemoveLight(int index);
 
+			// Sets Ambient Light colour
 			void SetAmbient(Color ambientColor);
+
+			// Sets Ambient Light intensity
 			void SetAmbient(float ambientIntensity);
+
+			// Sets Ambient Light colour & intensity
 			void SetAmbient(Color ambientColor, float ambientIntensity);
 		};
 	}
