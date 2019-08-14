24157f6a5174e72145f08d9cfc2a39866a9687dd
Tomasz Zając <tomezpl@gmail.com>
2019-08-14 22:28:55 +0100

    Adds a Default Texture fallback to prevent shader errors when a texture doesn't get passed

--- Lepus3D/Source/Texture.h
+++ Lepus3D/Source/Texture.h
@@ -14,7 +14,8 @@ namespace LepusEngine
 		public:
 			Texture2D() { m_Data = nullptr; m_Width = m_Height = m_Size = m_Channels = 0; }
 			Texture2D(char* fileName, char* directory = "../../Content");
-			void Load(char* fileName, char* directory = "../../Content");
+			bool Load(char* fileName, char* directory = "../../Content");
+			static Texture2D Default();
 			unsigned char* GetData();
 			int GetSize();
 			int GetWidth();
--- Lepus3D/Source/Texture/Texture2D.cpp
+++ Lepus3D/Source/Texture/Texture2D.cpp
@@ -1,5 +1,6 @@
 #include "../Texture.h"
 #include <string>
+#include <fstream>
 #define STB_IMAGE_IMPLEMENTATION // This is critcal as otherwise the build will fail due to unresolved externals
 #include "../3rdparty/stb_image.h"
 
@@ -10,17 +11,46 @@ Texture2D::Texture2D(char* fileName, char* dir)
 	this->Load(fileName, dir);
 }
 
-void Texture2D::Load(char* fN, char* dir)
+bool Texture2D::Load(char* fN, char* dir)
 {
 	std::string filePath = dir;
 	filePath.append("/");
 	filePath.append(fN);
+
+	// Check if file exists. If not, load the default texture (1x1 white).
+	std::ifstream file(filePath.c_str());
+
+	if(!file.good())
+	{
+		Texture2D default = Texture2D::Default();
+		m_Data = default.m_Data;
+		m_Size = default.m_Size;
+		m_Channels = default.m_Channels;
+		m_Width = default.m_Width;
+		m_Height = default.m_Height;
+
+		return false;
+	}
+	
 	m_Data = stbi_load(filePath.c_str(), &m_Width, &m_Height, &m_Channels, 4); // load RGBA texture
 
 	if(filePath.find(".tga") == std::string::npos)
 		stbi__vertical_flip(m_Data, m_Width, m_Height, 4); // non-TGA images need to be flipped vertically
 	
 	m_Size = m_Width * m_Height * sizeof(unsigned char) * 4;
+
+	return true;
+}
+
+Texture2D Texture2D::Default()
+{
+	Texture2D texture;
+	texture.m_Data = new unsigned char[4] {255, 255, 255, 255};
+	texture.m_Channels = 4;
+	texture.m_Size = sizeof(unsigned char) * 4; // TODO: is this sizeof really necessary?
+	texture.m_Width = texture.m_Height = 1;
+
+	return texture;
 }
 
 unsigned char* Texture2D::GetData()
