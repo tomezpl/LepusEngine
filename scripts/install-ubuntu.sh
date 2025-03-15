d4246ccd6741777f4f6d31d678fe5ea304c30789
Tomasz Zając <tomezpl@gmail.com>
2025-03-15 18:36:30 +0000

    ci: fixed missing vulkan apt sources

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# APT sources
+echo "Adding APT sources"
+wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-noble.list http://packages.lunarg.com/vulkan/lunarg-vulkan-noble.list
+
 # APT update
 echo "Updating APT index"
 sudo apt update
